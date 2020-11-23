b79bf0fe04d6faba9d651232c7f9974c361e445f
Jim Tan <jm.tan@outlook.com>
2020-11-23 17:05:03 +0800

    第一次提交

---
+++ basics/This_is_print.py
@@ -0,0 +1,7 @@
+# 文案删除
+print('This is print txt')
+# 运算表达式结果输出
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("What is 3 + 2?", 3 + 2)
